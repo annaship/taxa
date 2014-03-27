a06b966501baa1e9a19880d7e6b33e283f43f191
annaship <tetya-loshad@yandex.ru>
2014-03-27 15:40:54 -0400

    print queries for good species into file

--- check_genus_name.py
+++ check_genus_name.py
@@ -16,11 +16,7 @@ class Entry:
         self.genus_name               = self.genus_species.split()[0]
         self.species_name             = ""
         if (len(self.genus_species.split()) == 2):
-            self.species_name             = self.genus_species.split()[1]
-        # except:
-        #     print "genus_species with error: ", self.genus_species
-        #     print "Unexpected error:", sys.exc_info()[0]
-        #     raise
+            self.species_name         = self.genus_species.split()[1]
 
         
     def is_species(self):
@@ -28,16 +24,18 @@ class Entry:
             return self.species_name[0].islower() and re.search('[^a-z]', self.species_name) == None
         
     def good_gen(self):
+        query = ""    
         if (self.number_of_ranks == 6 and self.last_rank_from_full_name == self.genus_name):
-            print "GOOD: number_of_ranks = %s, last_rank_from_full_name = %s, genus_name = %s\nself.full_name = %s\ngenus_species = %s\n" % (self.number_of_ranks, self.last_rank_from_full_name, self.genus_name, self.full_name, self.genus_species)    
-            print "HERE: self.is_species()  = %s\n" % self.is_species() 
+            # print "GOOD: number_of_ranks = %s, last_rank_from_full_name = %s, genus_name = %s\nself.full_name = %s\ngenus_species = %s\n" % (self.number_of_ranks, self.last_rank_from_full_name, self.genus_name, self.full_name, self.genus_species)    
+            # print "HERE: self.is_species()  = %s\n" % self.is_species() 
             if self.is_species():
                 new_name = self.full_name + ";" + self.species_name
                 print "NEW name = %s\n" % new_name                
                 
-                query = 'UPDATE refssu_115_from_file SET taxonomy = "%s", taxslv_silva_modification = concat(taxslv_silva_modification, "; add species name from fullname"), taxonomy_source = "taxslv_silva_modification" WHERE taxslv_silva_modified = "%s" and silva_fullname = "%s" AND deleted = 0 AND taxonomy = "" ' % (new_name, self.full_name, self.genus_species)
+                query = 'UPDATE refssu_115_from_file SET taxonomy = "%s", taxslv_silva_modification = concat(taxslv_silva_modification, "; add species name from fullname"), taxonomy_source = "taxslv_silva_modification" WHERE taxslv_silva_modified = "%s" and silva_fullname = "%s" AND deleted = 0 AND taxonomy = "";' % (new_name, self.full_name, self.genus_species)
                 print "query:\n", query
-                print "=" * 10
+                # print(query, file = self.add_species_file_name)
+        return query
 
     def print_bad_gen(self):
         if (self.number_of_ranks == 6 and self.last_rank_from_full_name != self.genus_name):
@@ -47,14 +45,24 @@ class Entry:
         pass
 
 
-fname = "mod_species3-27-14.csv"
+fname                 = "mod_species3-27-14.csv"
+add_species_file_name = "add_species.sql"
+all_queries          = []
+n = 0 
+
 with open(fname) as f:
     content = f.readlines()
     
 for line in content:
     try:
+        print n
+        print "=" * 10
+        n += 1
         row = Entry(line)
-        row.good_gen()
+        
+        print "row.good_gen() = %s" % row.good_gen()
+
+        all_queries.append(row.good_gen())
         # row.print_bad_gen()
         # print "number_of_ranks = %s, last_rank_from_full_name = %s, genus_name = %s\n" % (number_of_ranks, last_rank_from_full_name, genus_name)    
     except:
@@ -62,3 +70,16 @@ for line in content:
         print "Unexpected error:", sys.exc_info()[0]
         raise
     
+
+f = open(add_species_file_name, 'w')
+print "HERE11"
+print all_queries
+print "all_queries.type = ", type(all_queries)
+
+all_queries_str = "\n".join(map(str, all_queries))
+
+print all_queries_str
+print "all_queries_str.type = ", type(all_queries_str)
+
+f.write(all_queries_str)
+f.close()
