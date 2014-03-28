81074b3c8b5f13296821737b18ef45440cdd187a
annaship <tetya-loshad@yandex.ru>
2014-03-28 11:46:07 -0400

    move creation out of methods

--- check_genus_name.py
+++ check_genus_name.py
@@ -8,8 +8,15 @@ import re
 
 class Entry:
     def __init__(self, line):
-        self.full_name                = line.split(",")[0].strip()
-        self.genus_species            = line.split(",")[1].strip()
+        self.accession_id             = line.split(",")[0].strip()
+        self.full_name                = line.split(",")[1].strip()
+        try:
+            self.genus_species            = line.split(",")[2].strip()
+        except:
+            print "HERE1: line with error: ", line
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        
 
         self.number_of_ranks          = len(self.full_name.split(";"))
         self.last_rank_from_full_name = self.full_name.split(";")[-1]
@@ -37,30 +44,32 @@ class Entry:
 
 
     def print_bad_gen(self):
-        to_check = ""
+        # to_check = ""
         if (self.number_of_ranks == 6 and self.last_rank_from_full_name != self.genus_name and self.is_genus()):
             # print "BAD: number_of_ranks = %s, last_rank_from_full_name = %s, genus_name = %s\nself.full_name = %s\nself.genus_species = %s\n" % (self.number_of_ranks, self.last_rank_from_full_name, self.genus_name, self.full_name, self.genus_species)    
-            to_check = self.full_name + "," + self.genus_species + "," + self.genus_name
-        return to_check
+            # to_check = self.full_name + "," + self.genus_species + "," + self.genus_name + "," + self.last_rank_from_full_name
+            return True
+            # to_check
         
     def gen5(self):
-        to_check = ""
+        # to_check = ""
         if (self.number_of_ranks == 5 and self.is_genus()): 
-            to_check = self.full_name + "," + self.genus_species + "," + self.genus_name
-        return to_check
+            # to_check = self.full_name + "," + self.genus_species + "," + self.genus_name + "," + self.last_rank_from_full_name
+            return True
+            # to_check
         
 
 # main
 def print_good_species_into_file(): 
     f = open(add_species_file_name, 'w')
-    all_queries_str = "\n".join(map(str, all_queries))
+    all_queries_str = "\n".join(map(str, set(all_queries)))
     f.write(all_queries_str)
     f.close()
 
 def print_check_species_into_csv(list_name, filename_base):
     check_species_file_name = filename_base + ".csv"
     f = open(check_species_file_name, 'w')
-    f.write("taxslv_silva_modified,silva_fullname,genus_name")
+    f.write("taxslv_silva_modified,silva_fullname,genus_name,last_rank")
     list_name_str = "\n".join(map(str, set(list_name)))    
     f.write(list_name_str)
     f.close()
@@ -80,13 +89,23 @@ for line in content:
         # print n
         # print "=" * 10
         # n += 1
-        row = Entry(line)
+        row      = Entry(line)
+        to_check = ""
         
         # print "row.good_gen() = %s" % row.good_gen()
 
         all_queries.append(row.good_gen())
-        conflict_name.append(row.print_bad_gen())
-        all_to_check.append(row.gen5())
+        print "row.print_bad_gen() = %s, row.gen5 = %s" % (row.print_bad_gen(), row.gen5())
+        if (row.print_bad_gen()):
+            to_check = row.full_name + "," + row.genus_species + "," + row.genus_name + "," + row.last_rank_from_full_name            
+            conflict_name.append(to_check)
+            print "HERE11"
+        if (row.gen5()):
+            to_check = row.full_name + "," + row.genus_species + "," + row.genus_name + "," + row.last_rank_from_full_name            
+            all_to_check.append(to_check)
+            print "HERE22"
+        
+        # all_to_check.append(row.gen5())
     except:
         print "line with error: ", line
         print "Unexpected error:", sys.exc_info()[0]
